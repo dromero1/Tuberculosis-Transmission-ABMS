28159015af89e1887bc86a1ab2c614f3802075b8
David Andrés Romero Millán <romeromillandavidandres@gmail.com>
2019-03-11 21:11:28 -0500

    Update citizen agent

--- tubABM_prototype/src/agents/Citizen.java
+++ tubABM_prototype/src/agents/Citizen.java
@@ -4,7 +4,6 @@
 package agents;
 
 import java.util.List;
-
 import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.engine.schedule.ISchedulableAction;
 import repast.simphony.engine.schedule.ISchedule;
