71973600b4dd16fd74e8064ee13bba183998e365
David Andrés Romero Millán <romeromillandavidandres@gmail.com>
2019-04-23 12:26:14 -0500

    Add files via upload

--- tubABM/src/model/ModelParameters.java
+++ tubABM/src/model/ModelParameters.java
@@ -5,6 +5,12 @@ package model;
 
 public class ModelParameters {
 
+	// Simple conversions
+	public static final int HOURS_IN_DAY = 24;
+	public static final int HOURS_IN_WEEK = 168;
+	public static final int HOURS_IN_YEAR = 8760;
+	public static final int WEEKS_IN_YEAR = 52;
+	
 	// Intuition
 	public static final int INITIAL_WAKEUP_TIME = 5; // hour
 	public static final int FINAL_WAKEUP_TIME = 8; // hour
@@ -14,6 +20,7 @@ public class ModelParameters {
 	// Taken from ...
 	public static final double IMMUNODEFICIENCY_FOLD = 10;
 	public static final double RISK_FACTOR_ADJUSTMENT = 1.5;
+	public static final double INFECTION_PROGRESSION_RATE = 0.10 / HOURS_IN_YEAR;
 	public static final double TREATMENT_DURATION = 4320; // hours
 	public static final double TIME_TO_FULL_RECOVERY = 17520; // hours
 
@@ -22,11 +29,5 @@ public class ModelParameters {
 
 	// Taken from Noakes et al. (2006)
 	public static final double AVG_PULMONARY_VENTILATION_RATE = 0.48; // m3 per hour
-
-	// Simple conversions
-	public static final int HOURS_IN_DAY = 24;
-	public static final int HOURS_IN_WEEK = 168;
-	public static final int HOURS_IN_YEAR = 8760;
-	public static final int WEEKS_IN_YEAR = 52;
 
 }
